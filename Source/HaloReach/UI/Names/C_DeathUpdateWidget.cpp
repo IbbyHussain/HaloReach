370eb4a70135651e91e719fca8c973bbc263f5f4
IbbyHussain <58857365+IbbyHussain@users.noreply.github.com>
2021-09-05 11:47:53 +0100

    Global Death Updates
    created new widget class

---
+++ Source/HaloReach/UI/Names/C_DeathUpdateWidget.cpp
@@ -0,0 +1,9 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "HaloReach/UI/Names/C_DeathUpdateWidget.h"
+
+UC_DeathUpdateWidget::UC_DeathUpdateWidget(const FObjectInitializer& ObjectInitializer) : Super(ObjectInitializer)
+{
+
+}
---
+++ Source/HaloReach/UI/Names/C_DeathUpdateWidget.h
@@ -0,0 +1,22 @@
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "C_DeathUpdateWidget.generated.h"
+
+class UTextBlock;
+
+UCLASS()
+class HALOREACH_API UC_DeathUpdateWidget : public UUserWidget
+{
+	GENERATED_BODY()
+
+public:
+
+	UC_DeathUpdateWidget(const FObjectInitializer& ObjectInitializer);
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, meta = (BindWidget))
+	UTextBlock* DeathUpdateText;
+	
+};
