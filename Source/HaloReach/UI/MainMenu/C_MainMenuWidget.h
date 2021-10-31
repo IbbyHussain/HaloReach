d619cbd15f0d01ef54883a3461609944a5cd0a55
IbbyHussain <ibrahimhussain2002@gmail.com>
2021-10-30 14:15:54 -1200

    Main Menu / Host/Join
    - Added basic host and join via steam
    and main menu ui

---
+++ Source/HaloReach/UI/MainMenu/C_MainMenuWidget.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "C_MainMenuWidget.generated.h"
+
+UCLASS()
+class HALOREACH_API UC_MainMenuWidget : public UUserWidget
+{
+	GENERATED_BODY()
+
+
+
+	
+};
