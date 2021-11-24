e7b1a29ab6580ec2af1c95ab0638ceffbfa4501c
IbbyHussain <58857365+IbbyHussain@users.noreply.github.com>
2021-11-24 14:23:02 +0000

    Cardinal
    two more helper functions

--- Source/HaloReach/Components/C_CardinalDirectionsComponent.cpp
+++ Source/HaloReach/Components/C_CardinalDirectionsComponent.cpp
@@ -52,3 +52,84 @@ float UC_CardinalDirectionsComponent::RotationToCirlceDegrees(float Rotation)
 
 	return Rotation;
 }
+
+float UC_CardinalDirectionsComponent::GetDeltaClockwise(float A, float B, bool bIsClockwise)
+{
+	float MA = FGenericPlatformMath::Fmod(A, 360.0f);
+	float MB = FGenericPlatformMath::Fmod(B, 360.0f);
+
+	if(bIsClockwise)
+	{
+		if(MA > MB)
+		{
+			return 360.0f - (MA - MB);
+		}
+
+		else if(MA == MB)
+		{
+			return 0.0f;
+		}
+
+		else if(MA < MB)
+		{
+			return MB - MA;
+		}
+	}
+
+	else
+	{
+		if (MA > MB)
+		{
+			return MA - MB;
+		}
+
+		else if (MA == MB)
+		{
+			return 0.0f;
+		}
+
+		else if (MA < MB)
+		{
+			return 360.0f - (MB - MA);
+		}
+	}
+
+	return 0.0f;
+}
+
+FTranslationsOutput UC_CardinalDirectionsComponent::RotationToTranslation(FRotator A, FRotator B)
+{
+	float AYaw = A.Yaw;
+	float BYaw = B.Yaw;
+
+	// A stuff
+	float AYawDegrees = RotationToCirlceDegrees(AYaw);
+	float BYawDegrees = RotationToCirlceDegrees(BYaw);
+
+	float ClockwiseDelta = GetDeltaClockwise(AYawDegrees, BYawDegrees, true);
+	float AntiClockwiseDelta = GetDeltaClockwise(AYawDegrees, BYawDegrees, false);
+
+	if(ClockwiseDelta > AntiClockwiseDelta)
+	{
+		if( (AntiClockwiseDelta / PlayerCameraComp->FieldOfView) <= 1.0f)
+		{
+			float XValue = (FMath::Lerp(0.0f, MaxWidgetTranslation, AntiClockwiseDelta / PlayerCameraComp->FieldOfView)) * -1.0f;
+			return {FVector2D(XValue, 0.0f), false, true};
+		}
+
+		return{FVector2D(0.0f), false, false};
+	}
+
+	else if(ClockwiseDelta == AntiClockwiseDelta || ClockwiseDelta < AntiClockwiseDelta)
+	{
+		if((ClockwiseDelta / PlayerCameraComp->FieldOfView) <= 1.0f)
+		{
+			float XXValue = FMath::Lerp(0.0f, MaxWidgetTranslation, ClockwiseDelta / PlayerCameraComp->FieldOfView);
+			return{ FVector2D(XXValue, 0.0f), true, true };
+		}
+
+		return{ FVector2D(0.0f), true, false };
+	}
+
+	return { FVector2D(0.0f), false, false };
+}
--- Source/HaloReach/Components/C_CardinalDirectionsComponent.h
+++ Source/HaloReach/Components/C_CardinalDirectionsComponent.h
@@ -7,6 +7,21 @@
 #include "HaloReach/GlobalStructs.h"
 #include "C_CardinalDirectionsComponent.generated.h"
 
+USTRUCT(BlueprintType)
+struct FTranslationsOutput
+{
+	GENERATED_BODY()
+public:
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "Struct")
+	FVector2D Translation;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "Struct")
+	bool bIsClockwise;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "Struct")
+	bool bInRadarSight;
+
+};
 
 UCLASS(Blueprintable, ClassGroup = (Custom), meta = (BlueprintSpawnableComponent))
 class HALOREACH_API UC_CardinalDirectionsComponent : public UActorComponent
@@ -29,12 +44,23 @@ public:
 	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "Widget")
 	TArray<FDirectionsStruct> DirectionsArray;
 
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "Struct")
+	UCameraComponent* PlayerCameraComp;
+
+	float MaxWidgetTranslation = 332.5f;
+
 # pragma region Helper Functions
 
 	// UE4 uses rotation in range of 0 to 180 then -180 to 0.
 	// This function will convert that range to 0 to 360.
 	UFUNCTION(BlueprintCallable)
 	float RotationToCirlceDegrees(float Rotation);
+
+	UFUNCTION(BlueprintCallable)
+	float GetDeltaClockwise(float A, float B, bool bIsClockwise);
+
+	UFUNCTION(BlueprintCallable)
+	FTranslationsOutput RotationToTranslation(FRotator A, FRotator B);
 
 # pragma endregion
 };
