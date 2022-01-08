06182c25230e2c953428aa01a26929013c6416bb
IbbyHussain <ibrahimhussain2002@gmail.com>
2022-01-07 21:42:40 -1200

    Session UI
    barebones ui for creating session and selecting gamemode

---
+++ Source/HaloReach/UI/MainMenu/C_CreateGameWidget.h
@@ -0,0 +1,15 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "C_CreateGameWidget.generated.h"
+
+
+UCLASS()
+class HALOREACH_API UC_CreateGameWidget : public UUserWidget
+{
+	GENERATED_BODY()
+	
+};
